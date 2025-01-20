682a2542606f28e98cd61b4a53ca2d0734794d1a
Everton19 <evertontp5@gmail.com>
2025-01-20 00:28:48 -0300

    feature/day-2

--- nlw-05/src/app/page.tsx
+++ nlw-05/src/app/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+import { Header } from "@/components/Header";
+import { Player } from '../components/Player/index';
+import styles from '../styles/app.module.scss';
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    fetch('http://localhost:3333/episodes')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, []); 
+
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <h1>Index</h1>
+      </main>
+      <Player />
+    </div>
   );
 }
